f2b929c88e624071e36f71e455f781a88ffab790
7405321@gmail.com <7405321@gmail.com>
2014-06-30 12:06:26 +0800

    Initial Commit

---
+++ GongSuo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GongSuo
+//
+//  Created by 邓鹏 on 14-6-30.
+//  Copyright (c) 2014年 com.kevin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
